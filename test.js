d26b6a62154f7ce4a2673ca233e38198c20fe4b6
KaaL-EL <63342672+xKaaL-EL@users.noreply.github.com>
2023-04-18 19:59:52 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,184 +1,95 @@
 let question = [{
     num: 1,
-    title: "\"The convicted criminal absconded prior to the sentencing phase of the trial\". Here 'Absconded' means",
-    option: ["Touched the jury", "Reported immediately", "Left after discussion", "Departed secretly"],
-    ans: "d"
+    title: "\"print 39//2\" equals",
+    option: ["19", "19.5", "19.0", "Error"],
+    ans: "a"
   },
   {
     num: 2,
-    title: "\"The cathode of a battery was removed\". Here 'Cathode' means",
-    option: ["Positive pole", "Negative pole", "Neutral pole", "Opposite pole"],
-    ans: "b"
+    title: "print Hello World[::-1]",
+    option: ["dlroW olleH", "Hello Worl", "d", "Error"],
+    ans: "a"
   },
   {
     num: 3,
-    title: "\"The attorney accused the witness of defaming the defendant\". Here 'Defaming' means",
-    option: ["Killing", "Badgering", "Suffocating", "Slandering"],
-    ans: "d"
-  },
-  {
-    num: 4,
-    title: "\"The scientist was able to evoke powerful emotions from her audience\". Here 'Evoke' means",
-    option: ["Sell", "Calm", "Call forth", "Exaggerate"],
+    title: "The module in Python that supports regular expressions is",
+    option: ["regularEx", "pyregX", "regex", "None"],
     ans: "c"
   },
   {
+    num: 4,
+    title: "What does ~~~~~~5 evaluate to?",
+    option: ["11", "-1", "-5", "5"],
+    ans: "d"
+  },
+  {
     num: 5,
-    title: "You won't get very wet if you leave now as it's only _______",
-    option: ["pouring", "drizzling", "damping", "sliding"],
-    ans: "b"
+    title: "Which of these is not a core data type?",
+    option: ["Lists", "Dictionary", "Tuples", "Class"],
+    ans: "d"
   },
   {
     num: 6,
-    title: "He is on holiday _____ dubai now.",
-    option: ["in", "at", "to", "for"],
+    title: "What data type is the object below ? X = [90, 65, ‘hello’, -23]",
+    option: ["Lists", "Dictionary", "Tuples", "Class"],
     ans: "a"
   },
   {
     num: 7,
-    title: "We ____ done it yet.",
-    option: ["don't", "didn't", "hasn't", "haven't"],
-    ans: "d"
+    title: "To start Python from the command prompt, we use the command ______",
+    option: ["execute python", "python", "go python", "run pyhton"],
+    ans: "b"
   },
   {
     num: 8,
-    title: "They ____ rung back yet.",
-    option: ["have", "has", "haven't", "hasn't"],
+    title: "Which of the following is correct about Python?",
+    option: ["It supports automatic garbage collection", "It can be easily integrated with C, C++, COM, ActiveX, CORBA, and Java", "Both", "None"],
     ans: "c"
   },
   {
     num: 9,
-    title: "____ you learn it when you were at school?",
-    option: ["Do", "Does", "Did", "Have"],
-    ans: "c"
+    title: "L = ['a','b','c','d']. Now printing with join() function will give what?",
+    option: ["['a','b','c','d']", "abcd", "Error", "None"],
+    ans: "b"
   },
   {
     num: 10,
-    title: "I ____ had time to do it yet.",
-    option: ["didn't", "haven't", "hasn't", "don't"],
+    title: "Which of the following is correct about Python?",
+    option: ["Statically Typed", "Dynamically Typed", "Loosely Typed", "None"],
     ans: "b"
   },
   {
     num: 11,
-    title: "I didn't expect that she ____ be there.",
-    option: ["will", "would", "be", "been"],
-    ans: "b"
+    title: "Python is a case-sensitive language?",
+    option: ["Yes", "No", "Can't be answered", "Not Valid"],
+    ans: "a"
   },
   {
     num: 12,
-    title: "If you had to make a choice, what ____ you decide on?",
-    option: ["will", "would", "shall", "will have"],
-    ans: "b"
+    title: "Choose the best time complexity.",
+    option: ["O(log(n))", "O(n)", "O(n^2)", "O(n!)"],
+    ans: "a"
   },
   {
     num: 13,
-    title: "A ____ of sound advice",
-    option: ["bunch", "slice", "piece", "horde"],
-    ans: "c"
+    title: "Choose the worst time complexity",
+    option: ["O(2^N)", "O(n^7)", "O(n^(1/2))", "O(n)"],
+    ans: "a"
   },
   {
     num: 14,
-    title: "______ the better team, we lost the match.",
-    option: ["Despite of being", "Despite", "Despite being", "Although"],
+    title: "Which of the following best describes the useful criterion for comparing the efficiency of algorithms?",
+    option: ["Time Complexity", "Space Complexity", "Both", "None"],
     ans: "c"
   },
   {
     num: 15,
-    title: "The best synonym for the word 'futile': ",
-    option: ["animated", "petty", "pointless", "self-centered"],
-    ans: "c"
-  },
-  {
-    num: 16,
-    title: "The best synonym for the word 'ponder':",
-    option: ["sing loudly", "hold on", "put down", "think about"],
-    ans: "d"
-  },
-  {
-    num: 17,
-    title: "The best synonym for the word 'bleak':",
-    option: ["gentle", "unhealthily thin", "disgusting", "depressing"],
-    ans: "d"
-  },
-  {
-    num: 18,
-    title: "The best synonym for the word 'vitality':",
-    option: ["liveliness", "highlight", "joy", "courage"],
-    ans: "a"
-  },
-  {
-    num: 19,
-    title: "The best synonym for the word 'aisle':",
-    option: ["settlement", "cliff", "passage", "pond"],
-    ans: "c"
-  },
-  {
-    num: 20,
-    title: "To kill someone by hanging.",
-    option: ["Bluster", "Lynch", "Finagle", "Ordain"],
-    ans: "b"
-  },
-  {
-    num: 21,
-    title: "Where's the ______ post office, please?",
-    option: ["most near", "near", "more near", "nearest"],
-    ans: "d"
-  },
-  {
-    num: 22,
-    title: "We would never have had the accident if you ______ so fast.",
-    option: ["wouldn't been driving", "hadn't been driving", "had driven", "wouldn't drive"],
-    ans: "b"
-  },
-  {
-    num: 23,
-    title: "I'd like ______ information, please.",
-    option: ["an", "some", "piece", "a piece"],
-    ans: "b"
-  },
-  {
-    num: 24,
-    title: "We arrived ______ England two days ago.",
-    option: ["to", "in", "on", "at"],
-    ans: "b"
-  },
-  {
-    num: 25,
-    title: "If only I ______ richer.",
-    option: ["am", "were", "would be", "will be"],
-    ans: "c"
-  },
-  {
-    num: 26,
-    title: "Choose the one with a different meaning.",
-    option: ["thingy", "tank", "thing", "item"],
-    ans: "b"
-  },
-  {
-    num: 27,
-    title: "Choose the one with a different meaning.",
-    option: ["beside", "prior to", "in advance", "by"],
-    ans: "a"
-  },
-  {
-    num: 28,
-    title: "An area of a seawater separated by a reef or low sandbank.",
-    option: ["Coherence", "Rout", "Lagoon", "Underscore"],
-    ans: "c"
-  },
-  {
-    num: 29,
-    title: "A person who is unfairly blamed.",
-    option: ["Adobe", "Decorum", "Scapegoat", "Boychick"],
-    ans: "c"
-  },
-  {
-    num: 30,
-    title: "To be ________ means to have a good characteristic.",
-    option: ["Aesthetic", "Amicable", "Anachronistic", "Arid"],
+    title: "Algorithm A and B have a worst-case running time of O(Sqrt(n)) and O(logn), respectively. Therefore, algorithm B always runs faster than algorithm A.",
+    option: ["Yes", "No", "Same Speed","Can't be predicted"],
     ans: "a"
   }
 ];
+
 
 let abc = ["a", "b", "c", "d"];
 let sum = 0;
@@ -192,7 +103,7 @@ let finishBtn = document.getElementById("finish-btn");
 let timerDiv = document.querySelector(".timer");
 let resultBox = document.querySelector(".result-box");
 let index = 0;
-const eachTime = 30;
+const eachTime = 60;
 let timeLimit = question.length * eachTime;
 const fixedTime = timeLimit;
 // start quiz
